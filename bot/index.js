43c8e9328d154c61f972817d11ecd7451f29439c
Radaev Kirill Dmitrievich <kdradaev@vtb.ru>
2022-11-28 14:27:06 +0300

    github pages deploy links

--- bot/index.js
+++ bot/index.js
@@ -6,7 +6,7 @@ const TOKEN = '5574575386:AAEXCjS-cnsXyPvAG5KjJgsq8HSL5r191hI';
 const bot = new Telegraf(TOKEN);
 
 // Ссылка на деплой фронта
-const webAppUrl = 'https://github.com/evaliev/loan_web_app_bot';
+const webAppUrl = 'https://evaliev.github.io/loan_web_app_bot';
 
 bot.start((ctx) =>
   ctx.reply('Привет, кликай ниже и оставляй заявку на кредит', {
