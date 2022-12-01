c34c1237ee133ed411b18d8ca6985fa54589e8b9
Radaev Kirill Dmitrievich <kdradaev@vtb.ru>
2022-12-01 14:31:42 +0300

    Добавлена SuccessPage

---
+++ src/pages/SuccessPage/index.tsx
@@ -0,0 +1,27 @@
+import { useTelegramBtns } from '../../hooks';
+import { telegram } from '../../telegram';
+import { ReactComponent as CheckCircleIcon } from './CheckCircle.svg';
+import styles from './styles.module.scss';
+
+export const SuccessPage = () => {
+  useTelegramBtns({
+    mainBtnTitle: 'Вернуться в чат',
+    mainBtnHandler: () => {
+      telegram.close();
+    },
+    hasBackBtn: false,
+  });
+
+  return (
+    <div className={styles.container}>
+      <CheckCircleIcon />
+      <p className={styles.title}>Ваша заявка отправлена</p>
+      <p className={styles.subtitle}>
+        Вы успешно отправили заявку на экспресс-кредит!
+      </p>
+      <p className={styles.subtitle}>
+        Решение мы пришлем в чат-бот в течение 5 минут.
+      </p>
+    </div>
+  );
+};
